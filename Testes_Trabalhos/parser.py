8c503281fbb66ffe52977cef16aa4b09956c6dcc
Lorena Mamede <loremamede@gmail.com>
2022-07-23 18:14:47 -0300

    [Trabalho 3] Refatorando parser
    Modificando parser para uso genérico com captura de argumentos
    pela linha de comando.

--- Testes_Trabalhos/parser.py
+++ Testes_Trabalhos/parser.py
@@ -1,8 +1,8 @@
-import json
+import json, sys, getopt
 
-DIRETORIO = "./testes/testes3"
 
-def parsear_input(arquivo):
+
+def parsear_input(arquivo, diretorio, encoding):
     f = open(arquivo)
     lines = f.readlines()
 
@@ -18,8 +18,8 @@ def parsear_input(arquivo):
 
     for line in lines:
         if line.startswith("Test "):
-            test_file_input = DIRETORIO+"/Teste"+line.split()[1].replace(":", "")+'.txt'
-            test_file_out = DIRETORIO+"/Teste"+line.split()[1].replace(":", "")+'-out.txt'
+            test_file_input = "Teste"+line.split()[1].replace(":", "")+'.txt'
+            test_file_out = "Teste"+line.split()[1].replace(":", "")+'-out.txt'
 
             print(f"Criando arquivo ${test_file_input}\n")
 
@@ -47,23 +47,36 @@ def parsear_input(arquivo):
         
         elif isInput:
             print(f"Abrindo ${test_file_input} para Input de ${line}")
-            with open(test_file_input, 'a+') as f:
+            with open(diretorio + test_file_input, 'a+') as f:
                 f.write(line)
         
         elif isOutput:
-            with open(test_file_out, 'a+') as f:
+            with open(diretorio + test_file_out, 'a+') as f:
                 f.write(line)
     
     cenarios["testes"] = testes
-    return json.dumps(cenarios, indent=4, ensure_ascii=False).encode('utf-8')
+    return json.dumps(cenarios, indent=4, ensure_ascii=False).encode(encoding)
 
 
-def criar_cenarios_json(json_string):
-    with open(DIRETORIO + '/cenarios.json', 'w', encoding='utf-8') as outfile:
-        outfile.write(json_string.decode('utf-8'))
+def criar_cenarios_json(json_string, diretorio, encod):
+    with open(diretorio + 'cenarios.json', 'w', encoding=encod) as outfile:
+        outfile.write(json_string.decode(encod))
 
    
-def main():
-    json_string = parsear_input(DIRETORIO+"/entradas.txt")
-    criar_cenarios_json(json_string)
-main()
+def main(argv):
+    diretorio = ''
+    encod = 'utf-8'
+
+    try:
+        opts, args = getopt.getopt(argv, "d:")
+    except getopt.GetoptError:
+        print("parser.py -d <tests_dataset_dir>")
+    for opt, arg in opts:
+        if opt is "-d":
+            diretorio = arg + '/'
+
+    json_string = parsear_input(diretorio+"/entradas.txt", diretorio, encod)
+    criar_cenarios_json(json_string, diretorio, encod)
+
+if __name__ == "__main__":
+       main(sys.argv[1:])
